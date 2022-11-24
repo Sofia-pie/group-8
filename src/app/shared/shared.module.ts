ff51fc803ad05b33db3c24a857ddd6030d4b53d4
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2022-11-24 22:44:46 +0200

    add admin side panel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,26 @@ import { ProductCardComponent } from './product-card/product-card.component';
 import { BlogCardComponent } from './blog-card/blog-card.component';
 
 import { SearchPipe } from './pipes/search.pipe';
-import { RouterLink } from '@angular/router';
+
 import { SortPipe } from './pipes/sort.pipe';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ProductCardComponent, BlogCardComponent, SortPipe, SearchPipe],
-  imports: [CommonModule, RouterLink],
-  exports: [ProductCardComponent, BlogCardComponent, SortPipe, SearchPipe],
+  declarations: [
+    ProductCardComponent,
+    BlogCardComponent,
+    SortPipe,
+    SearchPipe,
+    SidePanelComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    ProductCardComponent,
+    BlogCardComponent,
+    SortPipe,
+    SearchPipe,
+    SidePanelComponent,
+  ],
 })
 export class SharedModule {}
